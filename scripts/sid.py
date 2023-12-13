a550b96dafe4cc8482658971356d0fabb7cc6b0a
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-12-13 21:04:41 +0530

    Update sid.py

--- scripts/sid.py
+++ scripts/sid.py
@@ -4,33 +4,29 @@ import subprocess
 import snowflake.connector
 
 directory = "dbscripts/"
-pattern = r"^[vV]\d+\.\d+\.\d+__[a-zA-Z0-9_]+\.sql$"
+pattern = r"^[vV]\d+\.\d+\.\d+_[a-zA-Z0-9_]+\.sql$"
 
 for entry in os.scandir(directory):
     if entry.is_file():
         file_name = entry.name
         print(file_name)
-        if re.match(pattern, file_name):
-            if re.match(r"^[vV]\d+\.\d+\.\d+_[a-zA-Z0-9_]+\.sql$", file_name):
-                print(f"Skipping '{file_name}' based on the restricted version format.")
-                continue
-
-            print(f"File '{file_name}' matches the pattern. Proceeding with schemachange.")
-            print("Running schemachange")
-
-            # Build the full schemachange command
-            full_command = f'schemachange -f {directory} -a $SF_ACCOUNT -u $SF_USERNAME -r $SF_ROLE -w $SF_WAREHOUSE -d $SF_DATABASE -c $SF_DATABASE.SCHEMACHANGE.CHANGE_HISTORY --create-change-history-table'
-
-            # Run schemachange using subprocess.run
-            result = subprocess.run(full_command, shell=True, capture_output=True, text=True)
-
-            if result.returncode == 0:
-                print("schemachange executed successfully.")
-            else:
-                print(f"Error executing schemachange. Exit code: {result.returncode}")
-                print("Output:", result.stdout)
-                print("Errors:", result.stderr)
-
-        else:
-            print(f"File '{file_name}' does not match the pattern. Skipping schemachange.")
+        
+        if not re.match(pattern, file_name):
+            print(f"File '{file_name}' does not match the allowed pattern. Skipping schemachange.")
             continue
+
+        print(f"File '{file_name}' matches the allowed pattern. Proceeding with schemachange.")
+        print("Running schemachange")
+
+        # Build the full schemachange command
+        full_command = f'schemachange -f {directory} -a $SF_ACCOUNT -u $SF_USERNAME -r $SF_ROLE -w $SF_WAREHOUSE -d $SF_DATABASE -c $SF_DATABASE.SCHEMACHANGE.CHANGE_HISTORY --create-change-history-table'
+
+        # Run schemachange using subprocess.run
+        result = subprocess.run(full_command, shell=True, capture_output=True, text=True)
+
+        if result.returncode == 0:
+            print("schemachange executed successfully.")
+        else:
+            print(f"Error executing schemachange. Exit code: {result.returncode}")
+            print("Output:", result.stdout)
+            print("Errors:", result.stderr)
