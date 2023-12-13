77147753f4c13ff16ca0841dd5a0075403ce2943
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-12-13 20:39:41 +0530

    Update sid.py

--- scripts/sid.py
+++ scripts/sid.py
@@ -2,8 +2,10 @@ import os
 import re
 import subprocess
 import snowflake.connector
+
 directory = "dbscripts/"
 pattern = r"^[vV]\d+\.\d+\.\d+__[a-zA-Z0-9_]+\.sql$"
+
 for entry in os.scandir(directory):
     if entry.is_file():
         file_name = entry.name
@@ -11,7 +13,20 @@ for entry in os.scandir(directory):
         if re.match(pattern, file_name):
             print(f"File '{file_name}' matches the pattern. Proceeding with schemachange.")
             print("Running schemachange")
-            os.system('schemachange -f $GITHUB_WORKSPACE/dbscripts -a $SF_ACCOUNT -u $SF_USERNAME -r $SF_ROLE -w $SF_WAREHOUSE -d $SF_DATABASE -c $SF_DATABASE.SCHEMACHANGE.CHANGE_HISTORY --create-change-history-table')
+
+            # Build the full schemachange command
+            full_command = f'schemachange -f {directory} -a $SF_ACCOUNT -u $SF_USERNAME -r $SF_ROLE -w $SF_WAREHOUSE -d $SF_DATABASE -c $SF_DATABASE.SCHEMACHANGE.CHANGE_HISTORY --create-change-history-table'
+
+            # Run schemachange using subprocess.run
+            result = subprocess.run(full_command, shell=True, capture_output=True, text=True)
+
+            if result.returncode == 0:
+                print("schemachange executed successfully.")
+            else:
+                print(f"Error executing schemachange. Exit code: {result.returncode}")
+                print("Output:", result.stdout)
+                print("Errors:", result.stderr)
+
         else:
             print(f"File '{file_name}' does not match the pattern. Skipping schemachange.")
             continue
