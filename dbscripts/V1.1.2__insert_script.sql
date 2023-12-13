92e3fc968b5b10ad31adb6ccfd1dd30a89a71e81
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-12-13 20:28:28 +0530

    Update V1.1.2__insert_script.sql

--- dbscripts/V1.1.2__insert_script.sql
+++ dbscripts/V1.1.2__insert_script.sql
@@ -1,1 +1,1 @@
-insert into SNOWFLAKE_CICD_SHOWCASE.public.fidelity_test_users values('payal','jindal','2020-01-01 12:23:23');
+insert into DEMO1_DB.public.fidelity_test_users values('payal','jindal','2020-01-01 12:23:23');
