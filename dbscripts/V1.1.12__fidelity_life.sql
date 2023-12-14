cf0804423b5532eb8d89a04fbb6c29ff26756c1e
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-12-14 21:49:48 +0530

    Update and rename V1.1.11__fidelity_life.sql to V1.1.12__fidelity_life.sql

--- dbscripts/V1.1.12__fidelity_life.sql
+++ dbscripts/V1.1.12__fidelity_life.sql
@@ -1,14 +1,14 @@
 use database DEMO1_DB;
 use schema PUBLIC;
 
-create table fidelity_test_users11 if not exists(
+create table fidelity_test_users12 if not exists(
   name varchar (100),  -- variable string column
   preferences string, -- column used to store JSON type of data
   created_at timestamp
 );
 
 
-create table fidelity_users11 if not exists(
+create table fidelity_users12 if not exists(
   name varchar (100),  -- variable string column
   preferences string, -- column used to store JSON type of data
   created_at timestamp
