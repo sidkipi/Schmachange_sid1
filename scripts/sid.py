1e85974b55f8b761d3ad6da187addd11567db8ad
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-12-14 21:49:24 +0530

    Update sid.py

--- scripts/sid.py
+++ scripts/sid.py
@@ -1,13 +1,13 @@
 import os
+import re
 
 valid = "false"
+pattern1 = re.compile(r'^R__[a-zA-Z]+(?:_[a-zA-Z0-9]+)+\.(sql|SQL)$')
+pattern2 = re.compile(r'^[vV]\d+\.\d+\.\d+__[a-zA-Z0-9_]+\.sql$')
 
 for file_name in os.listdir("dbscripts"):
     if file_name.endswith(".sql"):
-        if file_name.startswith(("v", "V")) and file_name[1:].split("__", 1)[0].isdigit():
-            print(f"File '{file_name}' matches the pattern.")
-            valid = "true"
-        elif file_name.startswith("R__"):
+        if pattern1.match(file_name) or pattern2.match(file_name):
             print(f"File '{file_name}' matches the pattern.")
             valid = "true"
         else:
