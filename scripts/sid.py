5a7f3affc3fd144a30f901c47bb6c1df903fe960
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-12-14 21:44:56 +0530

    Update sid.py

--- scripts/sid.py
+++ scripts/sid.py
@@ -1,35 +1,16 @@
 import os
-import re
 
-def validate_file_name(file_name):
-    pattern1 = r"^[vV]\d+\.\d+\.\d+__[a-zA-Z0-9_]+\.sql$"
-    pattern2 = r"^R__[a-zA-Z]+(?:_[a-zA-Z0-9]+)+\.(sql|SQL)$"
+valid = "false"
 
-    if re.match(pattern1, file_name) or re.match(pattern2, file_name):
-        print(f"File '{file_name}' matches the pattern.")
-        return True
-    else:
-        print(f"Skipping file '{file_name}' based on the restricted version format.")
-        return False
+for file_name in os.listdir("dbscripts"):
+    if file_name.endswith(".sql"):
+        if file_name.startswith(("v", "V")) and file_name[1:].split("__", 1)[0].isdigit():
+            print(f"File '{file_name}' matches the pattern.")
+            valid = "true"
+        elif file_name.startswith("R__"):
+            print(f"File '{file_name}' matches the pattern.")
+            valid = "true"
+        else:
+            print(f"Skipping file '{file_name}' based on the restricted version format.")
 
-def main():
-    directory = "dbscripts/"
-    include_files = []
-    exclude_files = []
-
-    for entry in os.scandir(directory):
-        if entry.is_file():
-            file_name = entry.name
-            if validate_file_name(file_name):
-                include_files.append(file_name)
-            else:
-                exclude_files.append(file_name)
-
-    with open("include_files.txt", "w") as include_file:
-        include_file.write("\n".join(include_files))
-
-    with open("exclude_files.txt", "w") as exclude_file:
-        exclude_file.write("\n".join(exclude_files))
-
-if __name__ == "__main__":
-    main()
+print(f"::set-output name=valid::{valid}")
