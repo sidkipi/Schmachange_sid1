5e96946cc2bcfeec5850f5aaa932b4a896c725a4
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-12-14 21:14:09 +0530

    Update sid.py

--- scripts/sid.py
+++ scripts/sid.py
@@ -1,36 +1,35 @@
 import os
 import re
-import subprocess
-import snowflake.connector
 
-directory = "dbscripts/"
-pattern = r"^[vV]\d+\.\d+\.\d+\s*__[a-zA-Z0-9_]+\.sql$"
-custom_pattern = r"^R__[a-zA-Z]+(?:_[a-zA-Z0-9]+)+\.(sql|SQL)$"
+def validate_file_name(file_name):
+    pattern1 = r"^[vV]\d+\.\d+\.\d+__[a-zA-Z0-9_]+\.sql$"
+    pattern2 = r"^R__[a-zA-Z]+(?:_[a-zA-Z0-9]+)+\.(sql|SQL)$"
 
-for entry in os.scandir(directory):
-    if entry.is_file():
-        file_name = entry.name
-        print(file_name)
-        if not re.match(pattern, file_name) and not re.match(custom_pattern, file_name):
-            print(f"Skipping '{file_name}' based on the restricted version format.")
-            continue
+    if re.match(pattern1, file_name) or re.match(pattern2, file_name):
+        print(f"File '{file_name}' matches the pattern.")
+        return True
+    else:
+        print(f"Skipping file '{file_name}' based on the restricted version format.")
+        return False
 
-        if re.match(r"^[vV]\d+\.\d+\.\d+_\d+\s*\.sql$", file_name):
-            print(f"Skipping '{file_name}' based on the restricted version format.")
-            continue
+def main():
+    directory = "dbscripts/"
+    include_files = []
+    exclude_files = []
 
-        print(f"File '{file_name}' matches the pattern. Proceeding with schemachange.")
-        print("Running schemachange")
+    for entry in os.scandir(directory):
+        if entry.is_file():
+            file_name = entry.name
+            if validate_file_name(file_name):
+                include_files.append(file_name)
+            else:
+                exclude_files.append(file_name)
 
-        # Build the full schemachange command
-        full_command = f'schemachange -f {directory} -a $SF_ACCOUNT -u $SF_USERNAME -r $SF_ROLE -w $SF_WAREHOUSE -d $SF_DATABASE -c $SF_DATABASE.SCHEMACHANGE.CHANGE_HISTORY --create-change-history-table'
+    with open("include_files.txt", "w") as include_file:
+        include_file.write("\n".join(include_files))
 
-        # Run schemachange using subprocess.run
-        result = subprocess.run(full_command, shell=True, capture_output=True, text=True)
+    with open("exclude_files.txt", "w") as exclude_file:
+        exclude_file.write("\n".join(exclude_files))
 
-        if result.returncode == 0:
-            print("schemachange executed successfully.")
-        else:
-            print(f"Error executing schemachange. Exit code: {result.returncode}")
-            print("Output:", result.stdout)
-            print("Errors:", result.stderr)
+if __name__ == "__main__":
+    main()
