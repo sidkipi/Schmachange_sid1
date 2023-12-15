8f911789a6a32809d43f0722c3c00b652d1d01bf
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-12-15 18:24:01 +0530

    Update sid.py

--- scripts/sid.py
+++ scripts/sid.py
@@ -1,6 +1,6 @@
 import os
 import re
-
+import sys
 
 valid = "false"
 pattern1 = re.compile(r'^R__[a-zA-Z]+(?:_[a-zA-Z0-9]+)+\.(sql|SQL)$')
@@ -21,21 +21,6 @@ if valid == "true":
     print(f"::set-output name=valid::{valid}")
     # Optionally, you can print the matching files for reference
     print("Matching files:", matching_files)
-
-    # Trigger the workflow dispatch event
-    workflow_dispatch_url = 'https://api.github.com/repos/siddevkipi/Schmachange_sid1/actions/workflows/main.yaml/dispatches'
-    headers = {
-        'Authorization': 'Bearer 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',
-        'Accept': 'application/vnd.github.v3+json'
-    }
-    payload = {
-        'ref': 'main'
-    }
-    response = requests.post(workflow_dispatch_url, headers=headers, json=payload)
-    if response.status_code == 204:
-        print("Workflow dispatch event triggered successfully.")
-    else:
-        print(f"Failed to trigger workflow dispatch event. Status code: {response.status_code}")
 else:
     print("No matching files found. Exiting with success.")
     sys.exit(0)
