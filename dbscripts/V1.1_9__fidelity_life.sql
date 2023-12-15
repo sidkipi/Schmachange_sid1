4eef5c87b122a01927f6c9fa13da102064743147
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-12-15 17:57:04 +0530

    Create V1.1_9__fidelity_life.sql

---
+++ dbscripts/V1.1_9__fidelity_life.sql
@@ -0,0 +1,8 @@
+use database SNOWFLAKE_CICD_SHOWCASE;
+use schema PUBLIC;
+
+create table fidelity_test_users if not exists(
+  name varchar (100),  -- variable string column
+  preferences string, -- column used to store JSON type of data
+  created_at timestamp
+);
