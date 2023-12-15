805dcf9139fe704c1a71cc692f971ce4256d1d3c
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-12-15 15:33:57 +0530

    Update sid.py

--- scripts/sid.py
+++ scripts/sid.py
@@ -5,12 +5,13 @@ import sys
 valid = "false"
 pattern1 = re.compile(r'^R__[a-zA-Z]+(?:_[a-zA-Z0-9]+)+\.(sql|SQL)$')
 pattern2 = re.compile(r'^[vV]\d+\.\d+\.\d+__[a-zA-Z0-9_]+\.sql$')
+pattern3 = re.compile(r'^A__[a-zA-Z]+(?:_[a-zA-Z0-9]+)+\.(sql|SQL)$')
 
 matching_files = []
 
 for file_name in os.listdir("dbscripts"):
     if file_name.endswith(".sql"):
-        if pattern1.match(file_name) or pattern2.match(file_name):
+        if pattern1.match(file_name) or pattern2.match(file_name) or pattern3.match(file_name):
             print(f"File '{file_name}' matches the pattern.")
             matching_files.append(file_name)
             valid = "true"
