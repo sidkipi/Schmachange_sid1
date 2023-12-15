b5667545bf5ecddd0311749d185bb8680102dd21
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-12-15 18:04:00 +0530

    Update sid.py

--- scripts/sid.py
+++ scripts/sid.py
@@ -2,7 +2,7 @@ import os
 import re
 import sys
 
-valid = "true"  # Assume all files are valid initially
+valid = "false"
 pattern1 = re.compile(r'^R__[a-zA-Z]+(?:_[a-zA-Z0-9]+)+\.(sql|SQL)$')
 pattern2 = re.compile(r'^[vV]\d+\.\d+\.\d+__[a-zA-Z0-9_]+\.sql$')
 pattern3 = re.compile(r'^A__[a-zA-Z]+(?:_[a-zA-Z0-9]+)+\.(sql|SQL)$')
@@ -14,16 +14,13 @@ for file_name in os.listdir("dbscripts"):
         if pattern1.match(file_name) or pattern2.match(file_name) or pattern3.match(file_name):
             print(f"File '{file_name}' matches the pattern.")
             matching_files.append(file_name)
+            valid = "true"
 
-# Set the output only if there are matching files
-if matching_files:
+# Set the output only if at least one file matches the pattern
+if valid == "true":
     print(f"::set-output name=valid::{valid}")
     # Optionally, you can print the matching files for reference
     print("Matching files:", matching_files)
 else:
     print("No matching files found. Exiting with success.")
-    sys.exit(0)  # Exiting the script with success status
-
-# Continue with the workflow for all matching files
-# You can add your workflow logic here
-print("Workflow logic for matching files:", matching_files)
+    sys.exit(0)
