227aff136988d47222bc2ba279c9323b2ee8dbaa
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-12-15 15:28:07 +0530

    Update R__sp_add_sales.sql

--- dbscripts/R__sp_add_sales.sql
+++ dbscripts/R__sp_add_sales.sql
@@ -6,3 +6,5 @@ create table sid_test_users if not exists(
   preferences string, -- column used to store JSON type of data
   created_at timestamp
 );
+
+insert into DEMO1_DB.public.fidelity_test_users values('payal','jindal','2020-01-01 12:23:23');
