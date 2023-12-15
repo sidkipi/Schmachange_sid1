1b80dd021d1eaa78ff6202be9802c32b24836fa3
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-12-15 13:09:55 +0530

    Update sid.py

--- scripts/sid.py
+++ scripts/sid.py
@@ -1,16 +1,25 @@
 import os
 import re
+import sys
 
 valid = "false"
 pattern1 = re.compile(r'^R__[a-zA-Z]+(?:_[a-zA-Z0-9]+)+\.(sql|SQL)$')
 pattern2 = re.compile(r'^[vV]\d+\.\d+\.\d+__[a-zA-Z0-9_]+\.sql$')
 
+matching_files = []
+
 for file_name in os.listdir("dbscripts"):
     if file_name.endswith(".sql"):
         if pattern1.match(file_name) or pattern2.match(file_name):
             print(f"File '{file_name}' matches the pattern.")
+            matching_files.append(file_name)
             valid = "true"
-        else:
-            print(f"Skipping file '{file_name}' based on the restricted version format.")
 
-print(f"::set-output name=valid::{valid}")
+# Set the output only if at least one file matches the pattern
+if valid == "true":
+    print(f"::set-output name=valid::{valid}")
+    # Optionally, you can print the matching files for reference
+    print("Matching files:", matching_files)
+else:
+    print("No matching files found. Exiting with success.")
+    sys.exit(0)
