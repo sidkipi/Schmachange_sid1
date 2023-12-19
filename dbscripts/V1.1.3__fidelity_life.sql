620a55b28532fd3afe20ab6ad0740ed73692d249
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-12-19 17:03:03 +0530

    Delete dbscripts directory

--- dbscripts/V1.1.3__fidelity_life.sql
+++
@@ -1,8 +0,0 @@
-use database DEMO1_DB;
-use schema PUBLIC;
-
-create table fi_test_users if not exists(
-  name varchar (100),  -- variable string column
-  preferences string, -- column used to store JSON type of data
-  created_at timestamp
-);
